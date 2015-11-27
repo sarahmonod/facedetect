24ea4b8f9e0f12a0d3d95ea2dcc59f2857ebe2d1
Gustave (Gus) Arthur Monod <gustave.monod@insa-lyon.fr>
2015-11-27 15:05:17 +0100

    Fixed ranges

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import numpy as np
-from PIL import Image
+from PIL import Image, ImageDraw
 import caffe
 
 # converts from RGB to greyscale
@@ -9,11 +9,11 @@ def rgb2gray(rgb):
     return gray
 
 def window(x, y, im):
-    return im[x:x+WINDOW_SIZE, y:y+WINDOW_SIZE]
+    return im[y:y+WINDOW_SIZE, x:x+WINDOW_SIZE]
 
 # parameters
 WINDOW_SIZE = 36
-DETECTION_THRESHOLD = 0.8
+DETECTION_THRESHOLD = 0.9
 PROTOTXT = 'NET.prototxt'
 CAFFE_MODEL = 'facenet_iter_200000.caffemodel'
 INPUT_IMAGE = 'samples/images/faces.jpg'
@@ -22,12 +22,12 @@ DETECTED_OUTPUT_PATH = 'samples/detected/'
 caffe.set_mode_cpu()
 net = caffe.Net(PROTOTXT, CAFFE_MODEL, caffe.TEST)
 
-im_array = np.array(Image.open(INPUT_IMAGE).convert('RGB'))
+image = Image.open(INPUT_IMAGE).convert('RGB')
+im_array = np.array(image)
 im_array = rgb2gray(im_array)
 
-# TODO check if bounds are correct (getting execution error at the end...)
-for x in range(0, im_array.size - WINDOW_SIZE):
-    for y in range(0, im_array[0].size - WINDOW_SIZE):
+for x in xrange(0, len(im_array[0]) - WINDOW_SIZE, 2):
+    for y in xrange(0, len(im_array) - WINDOW_SIZE, 2):
         window_array = window(x, y, im_array)
         im_input = window_array[np.newaxis, np.newaxis, :, :]
         net.blobs['data'].reshape(*im_input.shape)
@@ -36,10 +36,10 @@ for x in range(0, im_array.size - WINDOW_SIZE):
 
         if out['loss'][0][1] > DETECTION_THRESHOLD:
             print 'face detected at', x, ';', y, ' (', out['loss'][0][1], ')'
+            draw = ImageDraw.Draw(image)
+            draw.rectangle((x, y, x + WINDOW_SIZE, y + WINDOW_SIZE), fill=None, outline="red")
             Image.fromarray(window_array).convert('RGB').save(DETECTED_OUTPUT_PATH + 'face_%d_%d.pgm' % (x, y))
 
 
 
-
-
-
+image.show()
